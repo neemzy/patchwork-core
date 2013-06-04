b0fb2a4da7103dd67a748b921c01e3ec692e540a
Tom Panier <tom.panier@free.fr>
2013-06-04 11:26:52 +0200

    RedBean typeHasField helper universalization

--- src/Patchwork/Helper/RedBean.php
+++ src/Patchwork/Helper/RedBean.php
@@ -6,6 +6,7 @@ class RedBean extends \RedBean_Facade
 {
     public static function typeHasField($type, $field)
     {
-        return !! self::getCell('SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? AND COLUMN_NAME = ?', array($type, $field));
+        $columns = R::getColumns($type);
+        return isset($columns[$field]);
     }
 }
