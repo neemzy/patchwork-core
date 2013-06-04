e10d0f5c1c047d220cbc940ae79a2489414ec73e
Tom Panier <tom.panier@free.fr>
2013-06-04 14:18:29 +0200

    improved json responses status code handling (bis)

--- src/Patchwork/Controller/ApiController.php
+++ src/Patchwork/Controller/ApiController.php
@@ -46,7 +46,7 @@ class ApiController implements ControllerProviderInterface
         $ctrl->get(
             '/',
             function () use ($app, $class) {
-                $data = R::findAndExport($class, '1');
+                $data = R::findAndExport($class, 1);
                 return Tools::jsonResponse($data);
             }
         )->bind('api.'.$class.'.getAll');
@@ -58,8 +58,8 @@ class ApiController implements ControllerProviderInterface
         $ctrl->get(
             '/{id}',
             function ($id) use ($app, $class) {
-                $data = R::findAndExport($class, 'id=?', array($id));
-                return Tools::jsonResponse($data, (count($data) ? 200 : 404));
+                $data = R::findAndExport($class, 'id = ?', array($id));
+                return Tools::jsonResponse($data);
             }
         )->bind('api.'.$class.'.getOne')->assert('id', '\d+');
 
