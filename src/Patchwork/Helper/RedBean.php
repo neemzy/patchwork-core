a7576e30758f36530b1a63088773aed3fc36aae7
Tom Panier <tom.panier@free.fr>
2013-06-06 12:07:18 +0200

    catch exception on schema queries for non-existing tables

--- src/Patchwork/Helper/RedBean.php
+++ src/Patchwork/Helper/RedBean.php
@@ -6,7 +6,11 @@ class RedBean extends \RedBean_Facade
 {
     public static function typeHasField($type, $field)
     {
-        $columns = self::getColumns($type);
-        return isset($columns[$field]);
+        try {
+            $columns = self::getColumns($type);
+            return isset($columns[$field]);
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
