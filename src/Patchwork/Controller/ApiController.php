cd77fc73a7be46737e3c81d4a7a572d09991166b
neemzy <tom.panier@free.fr>
2013-11-10 16:39:03 +0100

    fulfilled restfulness of json api controller

--- src/Patchwork/Controller/ApiController.php
+++ src/Patchwork/Controller/ApiController.php
@@ -68,6 +68,89 @@ class ApiController implements ControllerProviderInterface
 
 
 
+        // Create/Update
+
+        $ctrl->match(
+            '/{id}',
+            function ($id) use ($app, $class) {
+                $id = +$id;
+                $bean = R::load($class, $id);
+
+                if ($id != +$bean_id) {
+                    $app->abort(404);
+                }
+
+                $asserts = $bean->getAsserts();
+                
+                foreach ($asserts as $key => $assert) {
+                    $bean->$key = $app['request']->get($key);
+                }
+
+                if ((R::typeHasField($class, 'position')) && (! $id)) {
+                    $position = R::getCell('SELECT position FROM '.$class.' ORDER BY position DESC LIMIT 1');
+                    $bean->position = $position + 1;
+                }
+
+                $code = $id ? 202 : 201;
+
+                try {
+                    R::store($bean);
+                    $response = $bean->export();
+                } catch (Exception $e) {
+                    $errors = $e->getDetails();
+                    $response = array('errors' => array());
+                    $code = 400;
+
+                    foreach ($errors as $error) {
+                        $response['errors'][$error->getPropertyPath()] = $app['translator']->trans($error->getMessage());
+                    }
+                }
+
+                return Tools::jsonResponse($response, $code);
+            }
+        )->bind('api.'.$class.'.post')->assert('id', '\d*')->method('POST|PUT');
+
+
+
+        // Delete
+
+        $ctrl->delete(
+            '/{id}',
+            function ($id) use ($app, $class) {
+                $bean = R::load($class, $id);
+
+                if (! $bean->id) {
+                    $app->abort(404);
+                }
+
+                if (R::typeHasField($class, 'position')) {
+                    R::exec('UPDATE '.$class.' SET position = position - 1 WHERE position > ?', array($bean->position));
+                }
+
+                if (R::typeHasField($class, 'image')) {
+                    $dir = BASE_PATH.'/public/assets/img/'.$class.'/';
+                    unlink($dir.$bean->image);
+                }
+
+                R::trash($bean);
+
+                return Tools::jsonResponse(null, 204);
+            }
+        )->bind('api.'.$class.'.delete')->assert('id', '\d+');
+
+
+
+        // Gotta catch'em all
+
+        $ctrl->match(
+            '{uri}',
+            function () use ($app) {
+                $app->abort(400);
+            }
+        )->assert('uri', '.*');
+
+
+
         return $ctrl;
     }
 }
