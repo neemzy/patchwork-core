0257bb23c14f9ba2853c955c8376bf2d6d9f1d5c
neemzy <tom.panier@free.fr>
2014-08-11 20:35:01 +0200

    new controller syntax for front one

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -23,43 +23,47 @@ class FrontController extends AbstractController
         /**
          * Homepage
          */
-        $ctrl->get(
-            '/',
-            function () use ($app) {
-                $root = str_replace('index.php/', '', $app['url_generator']->generate('home'));
+        $ctrl
+            ->get(
+                '/',
+                function () use ($app) {
+                    $root = str_replace('index.php/', '', $app['url_generator']->generate('home'));
 
-                if ($app['request']->getRequestURI() != $root) {
-                    return $app->redirect($root, Response::HTTP_MOVED_PERMANENTLY);
+                    if ($app['request']->getRequestURI() != $root) {
+                        return $app->redirect($root, Response::HTTP_MOVED_PERMANENTLY);
+                    }
+
+                    return $app['twig']->render('front/partials/home.twig');
                 }
-
-                return $app['twig']->render('front/partials/home.twig');
-            }
-        )->bind('home');
+            )
+            ->bind('home');
 
 
 
         /**
          * Admin root
          */
-        $ctrl->get(
-            '/admin',
-            function () use ($app) {
-                return $app->redirect($app['url_generator']->generate(ADMIN_ROOT));
-            }
-        );
+        $ctrl
+            ->get(
+                '/admin',
+                function () use ($app) {
+                    return $app->redirect($app['url_generator']->generate(ADMIN_ROOT));
+                }
+            );
 
 
         /**
          * robots.txt
          */
-        $ctrl->get(
-            '/robots.txt',
-            function () use ($app) {
-                $response = new Response('User-agent: *'.PHP_EOL.($app['debug'] ? 'Disallow: /' : 'Sitemap: '.$app['url_generator']->generate('home').'sitemap.xml'));
-                $response->headers->set('Content-Type', 'text/plain');
-                return $response;
-            }
-        );
+        $ctrl
+            ->get(
+                '/robots.txt',
+                function () use ($app) {
+                    $response = new Response('User-agent: *'.PHP_EOL.($app['debug'] ? 'Disallow: /' : 'Sitemap: '.$app['url_generator']->generate('home').'sitemap.xml'));
+                    $response->headers->set('Content-Type', 'text/plain');
+                    return $response;
+                }
+            );
 
         
         
