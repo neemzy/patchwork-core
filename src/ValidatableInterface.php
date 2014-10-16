af80b9c48281d9d61ab814074bf2da45d2b75f8a
neemzy <tom.panier@free.fr>
2014-10-16 23:01:08 +0200

    added phpdoc to interface

--- src/ValidatableInterface.php
+++ src/ValidatableInterface.php
@@ -6,5 +6,10 @@ use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 interface ValidatableInterface
 {
+    /**
+     * Valorizes model validation metadata
+     *
+     * @return void
+     */
     public static function loadValidatorMetadata(ClassMetadata $metadata);
 }
